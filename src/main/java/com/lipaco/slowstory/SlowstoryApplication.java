f269aa931775708aba4e47bf7f6457abcf38e089
insujie <ultra1945@gmail.com>
2021-12-06 13:46:52 +0900

    Initial commit

---
+++ src/main/java/com/lipaco/slowstory/SlowstoryApplication.java
@@ -0,0 +1,13 @@
+package com.lipaco.slowstory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlowstoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SlowstoryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lipaco/slowstory/SlowstoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lipaco.slowstory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlowstoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
