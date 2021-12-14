9c3848a399ea7834d2c36bdd798c45259e1b848c
insujie <ultra1945@gmail.com>
2021-12-14 11:45:14 +0900

    pass test

--- src/main/java/com/lipaco/slowstory/springboot/domain/user/UserRepository.java
+++ src/main/java/com/lipaco/slowstory/springboot/domain/user/UserRepository.java
@@ -1,8 +1,11 @@
 package com.lipaco.slowstory.springboot.domain.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findByEmail(String email);
